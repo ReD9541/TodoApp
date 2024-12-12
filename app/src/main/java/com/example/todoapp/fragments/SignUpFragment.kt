d83ac52ece5f45d5d3006232c11bb7d92653e758
Ritesh Dhungel <139316061+ReD9541@users.noreply.github.com>
2024-12-13 04:53:05 +1100

    Setup Firebase Auth setup

--- app/src/main/java/com/example/todoapp/fragments/SignUpFragment.kt
+++ app/src/main/java/com/example/todoapp/fragments/SignUpFragment.kt
@@ -5,22 +5,68 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.Navigator
 import com.example.todoapp.R
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import com.example.todoapp.databinding.SignupFragmentBinding
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
 
 class SignUpFragment : Fragment() {
 
+    private lateinit var auth:FirebaseAuth
+    private lateinit var navController: NavController
+    private lateinit var binding: SignupFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.signup_fragment, container, false)
+
+    ): View?  {
+
+        binding = SignupFragmentBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init(view)
+        registerEvents()
+    }
+
+    private fun init(view:View){
+        navController = Navigation.findNavController(view)
+        auth = FirebaseAuth.getInstance()
+    }
+
+    private fun registerEvents() {
+        binding.AuthTextView.setOnClickListener{
+            navController.navigate(R.id.action_signUpFragment_to_loginFragment)
+        }
+
+        binding.NextBtn.setOnClickListener{
+            val email = binding.EmailET.text.toString().trim()
+            val pass = binding.PasswordET.text.toString().trim()
+            val rePass = binding.ConfirmPasswordET.text.toString().trim()
+
+            if (email.isNotEmpty() && pass.isNotEmpty() && rePass.isNotEmpty()){
+                if (pass == rePass){
+                    auth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener(
+                        OnCompleteListener {
+                            if (it.isSuccessful){
+                                Toast.makeText(context ,"Registration Successful!! You may now Login..", Toast.LENGTH_SHORT).show()
+                                navController.navigate(R.id.action_signUpFragment_to_loginFragment)
+                            }else{
+                                Toast.makeText(context , it.exception?.message, Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    )
+                }
+            }
+
+        }
+    }
 }
